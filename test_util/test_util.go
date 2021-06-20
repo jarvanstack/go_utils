69d680606c0b12e8e1a636211f4978a3fda52ccd
jarvan <dengjiawen2530@qq.com>
2021-06-20 20:19:12 +0800

    jarvan/update add time formart

--- test_util/test_util.go
+++ test_util/test_util.go
@@ -15,35 +15,39 @@ import (
 //(2)[]LOOP TIMES
 //(3)[]QPS
 //------end---------
-const NS_TO_S = 1000000000
-const NS_TO_MS = 1000000
+const (
+	NS_TO_S  = 1000000000
+	NS_TO_MS = 1000000
+	TIME_FOMART = "2006-01-02-15:04:05"
+)
+
 type TU struct {
 	start   time.Time
 	n       uint32 //number of loop times.
 	comment string
 }
 
-func NewTestUtil(n uint32)*TU {
+func NewTestUtil(n uint32) *TU {
 	return &TU{n: n}
 }
-func (test *TU)Start()  {
+func (test *TU) Start() {
 	fmt.Printf("%s\n", "------run---------")
 	test.start = time.Now()
 }
-func (test *TU)StartWithComment(comment string)  {
+func (test *TU) StartWithComment(comment string) {
 	fmt.Printf("------run:%s---------\n", comment)
 	test.comment = comment
 	test.start = time.Now()
 }
-func  (test *TU)End()  {
+func (test *TU) End() {
 	end := time.Now()
 	useNano := end.UnixNano() - test.start.UnixNano()
 	//(1)[%ds][%dms][%dns] TIME
-	second := float64(useNano)/float64(NS_TO_S)
-	fmt.Printf("%s:(1)[%fs][%fms][%dns] TIME\n",test.comment,second, float64(useNano)/float64(NS_TO_MS),useNano)
+	second := float64(useNano) / float64(NS_TO_S)
+	fmt.Printf("%s:(1)[%fs][%fms][%dns] TIME\n", test.comment, second, float64(useNano)/float64(NS_TO_MS), useNano)
 	//(2)[%d]LOOP TIMES
-	fmt.Printf("%s:(2)[%d]LOOP TIMES\n",test.comment,test.n)
+	fmt.Printf("%s:(2)[%d]LOOP TIMES\n", test.comment, test.n)
 	//(3)[%d]QPS
-	fmt.Printf("%s:(3)[%d]QPS\n",test.comment,int32(float64(test.n)/second))
+	fmt.Printf("%s:(3)[%d]QPS\n", test.comment, int32(float64(test.n)/second))
 	fmt.Printf("%s\n", "------end---------")
 }
