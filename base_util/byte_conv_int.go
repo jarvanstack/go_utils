cdc6d17dceb1e1c36408178f6c3c9675060da0c2
jarvan <dengjiawen2530@qq.com>
2021-06-28 16:52:02 +0800

    jarvan/base_util bytes conv int

---
+++ base_util/byte_conv_int.go
@@ -0,0 +1,89 @@
+package base_util
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+//isSymbol表示有无符号
+func BytesToInt(b []byte, isSymbol bool)  (int, error){
+	if isSymbol {
+		return bytesToIntS(b)
+	}
+	return bytesToIntU(b)
+}
+
+
+//字节数(大端)组转成int(无符号的)
+func bytesToIntU(b []byte) (int, error) {
+	if len(b) == 3 {
+		b = append([]byte{0},b...)
+	}
+	bytesBuffer := bytes.NewBuffer(b)
+	switch len(b) {
+	case 1:
+		var tmp uint8
+		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
+		return int(tmp), err
+	case 2:
+		var tmp uint16
+		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
+		return int(tmp), err
+	case 4:
+		var tmp uint32
+		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
+		return int(tmp), err
+	default:
+		return 0,fmt.Errorf("%s", "BytesToInt bytes lenth is invaild!")
+	}
+}
+
+
+
+//字节数(大端)组转成int(有符号)
+func bytesToIntS(b []byte) (int, error) {
+	if len(b) == 3 {
+		b = append([]byte{0},b...)
+	}
+	bytesBuffer := bytes.NewBuffer(b)
+	switch len(b) {
+	case 1:
+		var tmp int8
+		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
+		return int(tmp), err
+	case 2:
+		var tmp int16
+		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
+		return int(tmp), err
+	case 4:
+		var tmp int32
+		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
+		return int(tmp), err
+	default:
+		return 0,fmt.Errorf("%s", "BytesToInt bytes lenth is invaild!")
+	}
+}
+
+
+//整形转换成字节
+func IntToBytes(n int,b byte) ([]byte,error) {
+	switch b {
+	case 1:
+		tmp := int8(n)
+		bytesBuffer := bytes.NewBuffer([]byte{})
+		binary.Write(bytesBuffer, binary.BigEndian, &tmp)
+		return bytesBuffer.Bytes(),nil
+	case 2:
+		tmp := int16(n)
+		bytesBuffer := bytes.NewBuffer([]byte{})
+		binary.Write(bytesBuffer, binary.BigEndian, &tmp)
+		return bytesBuffer.Bytes(),nil
+	case 3,4:
+		tmp := int32(n)
+		bytesBuffer := bytes.NewBuffer([]byte{})
+		binary.Write(bytesBuffer, binary.BigEndian, &tmp)
+		return bytesBuffer.Bytes(),nil
+	}
+	return nil,fmt.Errorf("IntToBytes b param is invaild")
+}
---
+++ base_util/byte_conv_int_test.go
@@ -0,0 +1,6 @@
+package base_util
+
+import "testing"
+
+func Test_bytes_to_int(t *testing.T) {
+}
