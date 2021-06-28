052ad757d645dc755f33eeaa97005b82901b5c8d
jarvan <dengjiawen2530@qq.com>
2021-06-28 19:52:49 +0800

    jarvan/base_util max const

--- base_util/byte_conv_int.go
+++ base_util/byte_conv_int.go
@@ -7,18 +7,28 @@ import (
 )
 
 //isSymbol表示有无符号
-func BytesToInt(b []byte, isSymbol bool)  (int, error){
+func BytesToInt(b []byte, isSymbol bool) (int, error) {
 	if isSymbol {
 		return bytesToIntS(b)
 	}
 	return bytesToIntU(b)
 }
 
+const (
+	Uint8Max = ^uint8(0)
+	Uint16Max = ^uint16(0)
+	Uint32Max = ^uint32(0)
+	Uint64Max = ^uint64(0)
+	Int8Max  = ^uint8(0) >> 1
+	Int16Max  = ^uint16(0) >> 1
+	Int32Max  = ^uint32(0) >> 1
+	Int64Max  = ^uint64(0) >> 1
+)
 
 //字节数(大端)组转成int(无符号的)
 func bytesToIntU(b []byte) (int, error) {
 	if len(b) == 3 {
-		b = append([]byte{0},b...)
+		b = append([]byte{0}, b...)
 	}
 	bytesBuffer := bytes.NewBuffer(b)
 	switch len(b) {
@@ -35,17 +45,14 @@ func bytesToIntU(b []byte) (int, error) {
 		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
 		return int(tmp), err
 	default:
-		return 0,fmt.Errorf("%s", "BytesToInt bytes lenth is invaild!")
+		return 0, fmt.Errorf("%s", "BytesToInt bytes lenth is invaild!")
 	}
 }
-
-
-
 
 //字节数(大端)组转成int(有符号)
 func bytesToIntS(b []byte) (int, error) {
 	if len(b) == 3 {
-		b = append([]byte{0},b...)
+		b = append([]byte{0}, b...)
 	}
 	bytesBuffer := bytes.NewBuffer(b)
 	switch len(b) {
@@ -62,7 +69,7 @@ func bytesToIntS(b []byte) (int, error) {
 		err := binary.Read(bytesBuffer, binary.BigEndian, &tmp)
 		return int(tmp), err
 	default:
-		return 0,fmt.Errorf("%s", "BytesToInt bytes lenth is invaild!")
+		return 0, fmt.Errorf("%s", "BytesToInt bytes lenth is invaild!")
 	}
 }
 
@@ -70,28 +77,58 @@ func bytesToIntS(b []byte) (int, error) {
 //int转换成byte[]（大端）
 //byteNumber 字节数组的数量.
 //强制支持 64 位,最大兼容 32 位
-func IntToBytes(n int, byteNumber uint8) ([]byte,error) {
+func IntToBytes(n int, byteNumber uint8) ([]byte, error) {
 	switch byteNumber {
 	case 1:
 		tmp := int8(n)
 		bytesBuffer := bytes.NewBuffer([]byte{})
 		err := binary.Write(bytesBuffer, binary.BigEndian, &tmp)
-		return bytesBuffer.Bytes(),err
+		return bytesBuffer.Bytes(), err
 	case 2:
 		tmp := int16(n)
 		bytesBuffer := bytes.NewBuffer([]byte{})
 		err := binary.Write(bytesBuffer, binary.BigEndian, &tmp)
-		return bytesBuffer.Bytes(),err
+		return bytesBuffer.Bytes(), err
 	case 4:
 		tmp := int32(n)
 		bytesBuffer := bytes.NewBuffer([]byte{})
 		err := binary.Write(bytesBuffer, binary.BigEndian, &tmp)
-		return bytesBuffer.Bytes(),err
+		return bytesBuffer.Bytes(), err
 	case 8:
 		tmp := int64(n)
 		bytesBuffer := bytes.NewBuffer([]byte{})
 		err := binary.Write(bytesBuffer, binary.BigEndian, &tmp)
-		return bytesBuffer.Bytes(),err
+		return bytesBuffer.Bytes(), err
 	}
-	return nil,fmt.Errorf("IntToBytes bit param is invaild")
+	return nil, fmt.Errorf("IntToBytes bit param is invaild")
+}
+
+//UintToBytes
+//uint转换成byte[]（大端）
+//byteNumber 字节数组的数量.
+//强制支持 64 位,最大兼容 32 位
+func UintToBytes(n uint64, byteNumber uint8) ([]byte, error) {
+	switch byteNumber {
+	case 1:
+		tmp := uint8(n)
+		bytesBuffer := bytes.NewBuffer([]byte{})
+		err := binary.Write(bytesBuffer, binary.BigEndian, &tmp)
+		return bytesBuffer.Bytes(), err
+	case 2:
+		tmp := uint16(n)
+		bytesBuffer := bytes.NewBuffer([]byte{})
+		err := binary.Write(bytesBuffer, binary.BigEndian, &tmp)
+		return bytesBuffer.Bytes(), err
+	case 4:
+		tmp := uint32(n)
+		bytesBuffer := bytes.NewBuffer([]byte{})
+		err := binary.Write(bytesBuffer, binary.BigEndian, &tmp)
+		return bytesBuffer.Bytes(), err
+	case 8:
+		tmp := uint64(n)
+		bytesBuffer := bytes.NewBuffer([]byte{})
+		err := binary.Write(bytesBuffer, binary.BigEndian, &tmp)
+		return bytesBuffer.Bytes(), err
+	}
+	return nil, fmt.Errorf("IntToBytes bit param is invaild")
 }
--- base_util/byte_conv_int_test.go
+++ base_util/byte_conv_int_test.go
@@ -6,6 +6,16 @@ import (
 )
 
 func Test_bytes_to_int(t *testing.T) {
-	bs, _ := Int64ToBytes(1<<10)
+	bs, _ := IntToBytes(1<<10,8)
 	fmt.Printf("bs=%b\n", bs)
+}
+func Test_int_max_const(t *testing.T)  {
+	fmt.Printf("Uint8Max=%d\n", Uint8Max)
+	fmt.Printf("Uint16Max=%d\n", Uint16Max)
+	fmt.Printf("Uint32Max=%d\n", Uint32Max)
+	fmt.Printf("Uint64Max=%d\n", Uint64Max)
+	fmt.Printf("Int8Max=%d\n", Int8Max)
+	fmt.Printf("Int16Max=%d\n", Int16Max)
+	fmt.Printf("Int32Max=%d\n", Int32Max)
+	fmt.Printf("Int64Max=%d\n", Int64Max)
 }
