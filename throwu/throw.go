ea9862a4c8b8ae2a03186984d02a02ca9898642d
jarvan <dengjiawen2530@qq.com>
2021-07-01 16:32:59 +0800

    jarvan/throwu trance()

--- throwu/throw.go
+++ throwu/throw.go
@@ -1,9 +1,25 @@
 package throwu
 
 import (
+	"fmt"
 	"github.com/fwhezfwhez/errorx"
+	"runtime"
+	"strings"
 )
-
+// Trace 返回错误的 trace 自己打印即可
+func Trace(err interface{}) string {
+	message := fmt.Sprintf("%s", err)
+	var pcs [32]uintptr
+	n := runtime.Callers(3, pcs[:]) // skip first 3 caller
+	var str strings.Builder
+	str.WriteString(message + "\nTraceback:")
+	for _, pc := range pcs[:n] {
+		fn := runtime.FuncForPC(pc)
+		file, line := fn.FileLine(pc)
+		str.WriteString(fmt.Sprintf("\n\t%s:%d", file, line))
+	}
+	return str.String()
+}
 func catch()  {
 	err := recover()
 	if err != nil {
