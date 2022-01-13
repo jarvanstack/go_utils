885b78b4f28a663ea8d4f123b07ae3f1e14e41b8
dengjiawen8955@gmail.com <dengjiawen8955@gmail.com>
2022-01-13 13:48:02 +0800

    fix(httpu):auth1 并没有解决跨域问题,但是将元素设置满了

--- httpu/cors.go
+++ httpu/cors.go
@@ -4,4 +4,9 @@ import "net/http"
 
 func CORSAll(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*") //允许访问所有域
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+	w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Max-Age", "360000")
 }
